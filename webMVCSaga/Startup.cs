e2cf10361daa94328c76b2f71a619a7cd833e204
DESKTOP-QBRBS0H\luisd <luisdark177@hotmail.com>
2018-10-15 10:27:23 -0500

    dasds

---
+++ webMVCSaga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webMVCSaga.Startup))]
+namespace webMVCSaga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
